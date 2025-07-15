c59226e2975f5b6d16d81fc9e52d4f70d090fee9
ALLWIN A <allwinallrich@gmail.com>
2025-07-15 10:23:05 +0530

    Update script.js

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ async function sendMessage() {
 
   // Call backend API
   try {
-    const response = await fetch("http://127.0.0.1:5000/chat", {
+    const response = await fetch("/chat", {
       method: "POST",
       headers: {
         "Content-Type": "application/json"
